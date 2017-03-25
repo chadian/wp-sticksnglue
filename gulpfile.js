f158f0fdef6afe9679ce885c6883e16a2916f768
Chad Carbert <chadcarbert@me.com>
2017-03-25 00:07:49 -0700

    Add css minifier and autoprefixer

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,16 @@
 
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var cleanCSS = require('gulp-clean-css');
 
 gulp.task('styles', function() {
   return gulp.src('./style/theme.scss')
     .pipe(sass({
       includePaths: require('node-normalize-scss').includePaths
     }).on('error', sass.logError))
+    .pipe(postcss())
+    .pipe(cleanCSS())
     .pipe(gulp.dest('.'));
 });
 
